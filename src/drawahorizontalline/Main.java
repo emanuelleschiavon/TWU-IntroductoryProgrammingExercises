7b43900f3b1d5b479b2b28d1576aeea45d8780ca
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-04-30 15:21:10 -0300

    Imprime linha horizontal

---
+++ src/drawahorizontalline/Main.java
@@ -0,0 +1,10 @@
+package drawahorizontalline;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 8;
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+    }
+}
