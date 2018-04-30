cd441c134a2f8f7ec95242579fa518b8941db4d1
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-04-30 15:21:38 -0300

    Imprime linha vertical

---
+++ src/drawaverticalline/Main.java
@@ -0,0 +1,11 @@
+package drawaverticalline;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 3;
+        for (int i = 0; i < number; i++) {
+            System.out.println("*");
+        }
+    }
+}
