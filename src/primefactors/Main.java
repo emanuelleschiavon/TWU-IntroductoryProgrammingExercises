ffee8fc730a4eef7596351d23be70c339ad65b69
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-05-02 13:35:54 -0300

    Imprime primos e divisores

--- src/primefactors/Main.java
+++ src/primefactors/Main.java
@@ -1,4 +1,31 @@
 package primefactors;
 
 public class Main {
+
+    public static void main(String[] args) {
+        generate(121);
+    }
+
+    public static void generate(int n) {
+        for (int i = 2; i <= n; i++) {
+            boolean isDivisible = n % i == 0;
+
+            if (isDivisible && isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
 }
